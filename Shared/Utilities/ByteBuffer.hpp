78b62a856af7cdb928eebdbdd553df157ae94ec8
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2022-09-26 17:05:57 -0300

    Shared: Added copyright header

--- Shared/Utilities/ByteBuffer.hpp
+++ Shared/Utilities/ByteBuffer.hpp
@@ -1,5 +1,23 @@
+/**
+ * MableEmulator is a server emulator for World of Warcraft
+ * Copyright (C) 2022 Saullo Bretas Silva
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #pragma once
 
+#include <array>
 #include <cstdint>
 #include <vector>
 
@@ -16,14 +34,18 @@ namespace Utilities
 
         ByteBuffer &operator<<(std::uint8_t value);
 
+        template <typename T> void append(T value);
+        void append(const std::uint8_t *value, std::size_t size);
+        template <std::size_t Size> void append(const std::array<std::uint8_t, Size> &value)
+        {
+            append(value.data(), Size);
+        }
+
     private:
         static constexpr auto initial_size = 4096;
 
         std::vector<std::uint8_t> m_data;
         std::size_t m_write_pos{0};
         std::size_t m_read_pos{0};
-
-        template <typename T> void append(T value);
-        void append(const std::uint8_t *value, std::size_t size);
     };
 } // namespace Utilities
