ad8d2643b54c4275d44e9e1f7e3ebc32ade0dd6b
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2022-09-27 10:11:10 -0300

    SQL: Added realmlist table

--- Database/Create/AuthDatabase.sql
+++ Database/Create/AuthDatabase.sql
@@ -20,3 +20,19 @@ CREATE TABLE `build_information` (
     `revision` INT DEFAULT NULL,
     PRIMARY KEY (`build`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `realmlist`;
+CREATE TABLE `realmlist` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(32) NOT NULL DEFAULT '',
+    `address` VARCHAR(255) NOT NULL DEFAULT '127.0.0.1',
+    `local_address` VARCHAR(255) NOT NULL DEFAULT '127.0.0.1',
+    `local_subnet_mask` VARCHAR(255) NOT NULL DEFAULT '255.255.255.0',
+    `port` SMALLINT UNSIGNED NOT NULL DEFAULT '8085',
+    `type` TINYINT UNSIGNED NOT NULL DEFAULT '0',
+    `flags` TINYINT UNSIGNED NOT NULL DEFAULT '2',
+    `category` TINYINT UNSIGNED NOT NULL DEFAULT '0',
+    `population` FLOAT UNSIGNED NOT NULL DEFAULT '0',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `index_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
